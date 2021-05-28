abee2412c0f5ed812ac5eba517f35b9745086e46
Harris Levine <acyclicgraphs@gmail.com>
2021-05-28 01:03:34 -0400

    Update Redeemable.test.ts

--- test/Redeemable.test.ts
+++ test/Redeemable.test.ts
@@ -38,4 +38,51 @@ describe('Redeemable', () => {
       it('The encryptionKey should belong to the user that provided it', async function () {
           
       })
+    
+    import { ethers } from 'hardhat';
+import { solidity } from 'ethereum-waffle';
+import chai from 'chai';
+import mocha from 'mocha';
+
+import { Redeemable } from '../typechain/ERC20Redeemable';
+
+chai.use(solidity);
+const { expect } = chai;
+
+describe('Redeemable', () => {
+    let signers: any;
+    let redeemable: Redeemable;
+    let owner: any;
+
+    beforeEach(async () => {
+        signers = await ethers.getSigners();
+        owner = signers[0];
+
+
+        redeemable = await ethers.getContractFactory('ERC20Redeemable', signers[0]);
+    
+    })
+    function findEvent(logs: any, eventName: string) {
+        for (let i = 0; i < logs.length; i++) {
+          if (logs[i].event === eventName) {
+            return logs[i];
+          }
+        }
+    
+        return null;
+      }
+
+      it('Ensure onlyOwner modifier prohibits all users but the owner to transact with the function', async function () {
+          
+      })
+    
+      it('The encryptionKey should belong to the user that provided it', async function () {
+          
+      })
+    
+      it('Users encrypted data is accepted by the redeem function', async function () {
+          
+      })
+})
+
 })
